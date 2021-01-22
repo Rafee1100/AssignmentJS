8e95c0538a972b131675aa7811b94a1615161a5d
Rafee 1100 <rafee.abdullah1@gmail.com>
2021-01-22 15:45:20 +0600

    some changes

--- assignment.js
+++ assignment.js
@@ -1,70 +1,77 @@
+//https://github.com/Rafee1100/AssignmentJS
 
 
 //Kilometer to Meter Converter
 
-function kilometerToMeter(kilometer){
+function kilometerToMeter(kilometer) {
     var meter;
-    if(kilometer>0){
-        meter=kilometer*1000;
+    if (kilometer >= 0) {
+        meter = kilometer * 1000;
         return meter;
     }
-    else{
+    else {
         return "Distance Can't be negative";
     }
 }
 
 //budgetCalculator function
 
-function budgetCalculator(watch,laptop,phone){
+function budgetCalculator(watch, phone, laptop) {
     var totalcost;
-    if(watch>=0 && laptop >=0 && phone>= 0){
-        totalcost=(50*watch)+(500*laptop)+(100*phone);
+    if (watch >= 0 && phone >= 0 && laptop >= 0) {
+        totalcost = (50 * watch) + (100 * phone) + (500 * laptop);
         return totalcost;
     }
-    else{
+    else {
         return "Amount of items can't be negative";
     }
 }
 
+
 //Hotel cost using hotelCost function
 
-function hotelCost(days){
+function hotelCost(days) {
     var hotelstaycost;
-    if(days>0){
-        if(days<=10){
-            hotelstaycost=100*days;           
+    if (days >= 0) {
+        if (days <= 10) {
+            hotelstaycost = 100 * days;
         }
-        else if(days<=20){
-            var firstpart=10*100;
-            var remaining=(days-10);
-            var secondpart=remaining*80;
-            hotelstaycost=firstpart+secondpart;
+        else if (days <= 20) {
+            var firstpart = 10 * 100;
+            var remaining = (days - 10);
+            var secondpart = remaining * 80;
+            hotelstaycost = firstpart + secondpart;
         }
-        else{
-            var firsttendayscost=10*100;
-            var secondtendayscost=10*80;
-            var remainingdays = days-20;
-            var thirdpartcost=remainingdays*50;
-            hotelstaycost=firsttendayscost+ secondtendayscost+thirdpartcost;
+        else {
+            var firsttendayscost = 10 * 100;
+            var secondtendayscost = 10 * 80;
+            var remainingdays = days - 20;
+            var thirdpartcost = remainingdays * 50;
+            hotelstaycost = firsttendayscost + secondtendayscost + thirdpartcost;
         }
         return hotelstaycost;
     }
-    else{
+    else {
         return "You have not been in the hotel.Welcome to Bay Resort have some cost!!"
     }
-   
+
 }
 
-//Megafriend using function
+//megaFriend using function that will find the maximum length name in an array
 
-function megafriend(friendsname){
-    var largestname=friendsname[0],i;
-    var l=largestname.length;
-    for(i=1;i<friendsname.length;i++){
-        var k=friendsname[i].length;
-        if(k>l){
-            largestname=friendsname[i];
+function megaFriend(friendsname) {
+    if (friendsname.length > 0) {
+        var largestname = friendsname[0], i;
+        for (i = 1; i < friendsname.length; i++) {
+            var l = largestname.length;
+            var k = friendsname[i].length;
+            if (k > l) {
+                largestname = friendsname[i];
+            }
         }
+        return largestname;
     }
-    return largestname;
+    if (friendsname.length == 0) {
+        return "There is no name in the Array.Have some person!"
+    }
 }
